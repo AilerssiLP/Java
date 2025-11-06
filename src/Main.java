3aadf5c2007fff3f1af07e1995ad37b9e16d96e4
jan.kolesar <jan.kolesar@student.tuke.sk>
2025-11-06 11:33:00 +0100

    Moving alg to class and starting it up from Main

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
     public static void main(String[] args) {
-
+        //System.out.println(args[1]);
         String num1 = args[0];
         String num2 = args[1];
+        alg1.run(num1, num2);
 
-        //System.out.println("Give me first number!");
-        //Scanner scanner = new Scanner(System.in);
-        //String name1 = scanner.nextLine();
-
-        String name1 = num1;
-        String[] digits1 = name1.split("");
-
-        //System.out.println("Give me second number!");
-        //Scanner scanner2 = new Scanner(System.in);
-
-        //String name2 = scanner2.nextLine();
-        String name2 = num2;
-
-        String[] digits2 = name2.split("");
-
-
-        //int length1 = name1.length();
-        //int length2 = name2.length();
-        int length1 = digits1.length;
-        int length2 = digits2.length;
-
-        int[] result2 = new int[length1 + length2];
-
-        for (int i = length2 - 1; i >= 0; i--) {
-            int d1 = Integer.parseInt(digits2[i]);
-            for (int j = length1 - 1; j >= 0; j--) {
-                int d2 = Integer.parseInt(digits1[j]);
-                int sum = result2[i + j + 1] + d1 * d2;
-                result2[i + j + 1] = sum % 10;
-                result2[i + j] += sum / 10;
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        int k = 0;
-        while (k < result2.length && result2[k] == 0) k++;
-        for (; k < result2.length; k++) sb.append(result2[k]);
-
-        if (sb.length() == 0) {
-            System.out.println("0");
-        } else {
-            System.out.println(sb.toString());
-        }
-
-        //scanner.close();
-        //scanner2.close();
     }
 }
+
